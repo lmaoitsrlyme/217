dc6a8715f550a650e6c04db997ee2c833f724aa1
lmaoitsrlyme <73641532+lmaoitsrlyme@users.noreply.github.com>
2022-11-19 20:11:27 +0800

    Add files via upload
    yourrmormorm

---
+++ public/script.js
@@ -0,0 +1,31 @@
+const socket = io("/");
+
+const user = prompt("Enter your name");
+
+$(function () {
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+    
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+})
+
+//totally didn't copy all this from my class code lmao
+
+socket.on("createMessage", (message, userName) => {
+    $(".messages").append(`
+        <div class="message">
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
+            <span>${message}</span>
+        </div>
+    `)
+});
